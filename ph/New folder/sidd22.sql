2a65f78c30858f584988dfa9df723252c59e6ecb
Vardhan S <147789596+vardhan179@users.noreply.github.com>
2023-12-03 19:48:40 +0530

    Uploaded the complete project

---
+++ ph/New folder/sidd22.sql
@@ -0,0 +1,48 @@
+INSERT INTO `player` 
+(`cap_no`, `age`, `dob`, `runs`, `wickets`, `type`, `no_of_matches`, `rank`, `batting_best`, `bowling_best`, `playername`, `image`, `teamname`) VALUES 
+        ('31','29','1993-10-09','13','5','Bowler','6','91','12','4-85','Olly Stone','ph/stone.png','ENGLAND'),
+        ('32','28','1994-02-21','18','33','Bowler','20','71','60*','6-24','Reece Topley','ph/topley.png','ENGLAND'),
+        ('33','32','1990-02-28','530','84','Allrounder','63','24','5-30','4-43','David Wiley','ph/wiley.png','ENGLAND'),
+       ('34', '26', '1994-07-01', '4700', '190', 'All-rounder', '120', '7', '100*', '4-50', 'Chris Woakes', 'ph/woakes.png', 'ENGLAND'),
+        ('41','26', '1994-03-25', '7000','100', 'Batsman', '120', '3', '250*', '0', 'Steve Smith', 'ph/smith.png', 'AUSTRALIA'),
+       ('42', '29', '1991-08-05', '7300', '160', 'Batsman', '130', '8', '190*', '0', 'David Warner', 'ph/warner.png', 'AUSTRALIA'),
+       ('43', '25', '1995-09-15', '5100', '170', 'Batsman', '100', '9', '160*', '0', 'Marnus Labuschagne', 'ph/labuschagne.png', 'AUSTRALIA'),
+       ('44', '27', '1993-04-30', '5800','180', 'Batsman', '110', '4', '180*', '0', 'Usman Khawaja', 'ph/khawaja.png', 'AUSTRALIA'),
+       ('45', '28', '1992-05-15', '6500', '120', 'Batsman', '140', '5', '220*', '0', 'Cameron Bancroft', 'ph/bancroft.png', 'AUSTRALIA'),
+       ('46', '30', '1990-06-01', '4800', '200', 'All rounder', '160', '6', '120*', '6/50', 'Marcus Stoinis', 'ph/stoinis.png', 'AUSTRALIA'),
+       ('47', '24', '1996-07-10', '4200', '130', 'All rounder', '90','7', '80*', '4/40', 'Ashton Agar', 'ph/agar.png', 'AUSTRALIA'),
+       ('48', '29', '1991-08-05', '7300', '160', 'Batsman', '130', '8', '190*',' 0', 'Travis Head', 'ph/head.png', 'AUSTRALIA'),
+       ('49', '25', '1995-09-15', '5100', '170', 'Batsman', '100', '9', '160*', '0', 'Matthew Wade', 'ph/wade.png', 'AUSTRALIA'),
+       ('50', '27', '1993-10-20', '6700', '180', 'Batsman', '120', '10', '210*', '0', 'Joe Burns', 'ph/burns.png', 'AUSTRALIA'),
+       ('51', '28', '1992-11-01', '4500', '190', 'All rounder', '140', '11', '110*', '5-30', 'Mitchell Starc', 'ph/starc.png', 'AUSTRALIA'),
+       ('52', '24', '1997-12-10', '4300', '150', 'All rounder', '90', '12', '70*', '4-40', 'Patrick Cummins', 'ph/cummins.png', 'AUSTRALIA'),
+       ('61','27', '1993-04-30', '5800',' 180', 'Batsman', '110',' 4', '180*', '0', 'Quinton de Kock', 'ph/dekock.png', 'SOUTH AFRICA'), 
+       ('62','29', '1991-08-05', '7300' , '160', 'Batsman', '130', '8', '190*', '0', 'Faf du Plessis', 'ph/duplessis.png', 'SOUTH AFRICA'),
+       ('63', '25', '1995-09-15', '5100' , '170', 'Batsman', '100', '9', '160*', '0', 'Temba Bavuma', 'ph/bavuma.png', 'SOUTH AFRICA'),
+       ('64', '26', '1994-03-25', '7000' , '100', 'Batsman', '120', '3', '250*', '0', 'Rassie van der Dussen', 'ph/vanderdussen.png', 'SOUTH AFRICA'),
+       ('65', '28', '1992-05-15', '6500' ,' 120', 'Batsman', '140',' 5', '220*', '0', 'Aiden Markram', 'ph/markram.png', 'SOUTH AFRICA'),
+       ('66', '30', '1990-06-01', '4800' , '200', 'All rounder', '160', '6', '120*', '6-50', 'Andile Phehlukwayo', 'ph/phehlukwayo.png', 'SOUTH AFRICA'),
+       ('67', '24', '1996-07-10', '4200' , '130', 'All rounder', '90', '7', '80*', '4-40', 'Kagiso Rabada', 'ph/rabada.png', 'SOUTH AFRICA'),
+       ('68', '29', '1991-08-05', '7300' , '160', 'Batsman', '130', '8', '190*', '0', 'Dwaine Pretorius', 'ph/pretorius.png', 'SOUTH AFRICA'),
+       ('69', '25', '1995-09-15', '5100'  , '180', 'Batsman', '120', '10', '210*', '0', 'Heinrich Klaasen', 'ph/klaasen.png', 'SOUTH AFRICA'),
+       ('71', '28', '1992-11-01', '4500' , '190', 'All rounder', '140', '11', '110*', '5-30', 'Lungi Ngidi', 'ph/ngidi.png', 'SOUTH AFRICA'),
+       ('72', '24', '1997-12-10', '4300' , '150', 'All rounder', '90', '12', '70*', '4-40', 'Anrich Nortje', 'ph/nortje.png', 'SOUTH AFRICA'),     
+        ('81', '27', '1993-04-30', '5800', '180', 'batsman', '110', '4', '180*',' 0', 'Kane Williamson', 'ph/williamson.png', 'NEW ZEALAND'),
+        ('82', '29', '1991-08-05', '7300', '160', 'batsman', '130', '8', '190*', '0', 'Ross Taylor', 'ph/taylor.png', 'NEW ZEALAND'),
+       ('83', '25', '1995-09-15', '5100', '170', 'batsman', '100', '9', '160*',' 0', 'Tom Latham', 'ph/latham.png', 'NEW ZEALAND'),
+       ('84', '26', '1994-03-25', '7000', '100', 'batsman', '120', '3', '250*', '0', 'Henry Nicholls', 'ph/nicholls.png', 'NEW ZEALAND'),
+       ('85', '28', '1992-05-15', '6500', '120', 'batsman', '140', '5', '220*', '0', 'Martin Guptill', 'ph/guptill.png', 'NEW ZEALAND'),
+       ('86', '30', '1990-06-01', '4800', '200', 'all rounder', '160', '6', '120*', '6-50', 'James Neesham', 'ph/neesham.png', 'NEW ZEALAND'),
+       ('87', '24', '1996-07-10', '4200', '130', 'all rounder', '90',' 7', '80*', '4-40', 'Colin de Grandhomme', 'ph/degrandhomme.png', 'NEW ZEALAND'),
+       ('88', '29', '1991-08-05', '7300', '160', 'batsman', '130', '8', '190*', '0', 'Tom Blundell', 'ph/blundell.png', 'NEW ZEALAND'),
+       ('89', '25', '1995-09-15', '5100', '170', 'batsman', '100', '9', '160*', '0', 'BJ Watling', 'ph/watling.png', 'NEW ZEALAND'),
+       ('90', '27', '1993-10-20', '6700', '180', 'batsman', '120', '10', '210*', '0', 'Daryl Mitchell', 'ph/mitchell.png', 'NEW ZEALAND'),
+       ('91', '28', '1992-11-01', '4500', '190', 'all rounder', '140',' 11', '110*', '5-30', 'Trent Boult', 'ph/boult.png', 'NEW ZEALAND'),
+       ('92','24', '1997-12-10', '4300', '150', 'all rounder', '90', '12', '70*', '4-40', 'Tim Southee', 'ph/southee.png', 'NEW ZEALAND');
+
+ 
+
+
+
+
+
---
+++ ph/New folder/siddarth.sql
@@ -0,0 +1,44 @@
+INSERT INTO `player` 
+(`cap_no`, `age`, `dob`, `runs`, `wickets`, `type`, `no_of_matches`, `rank`, `batting_best`, `bowling_best`, `playername`, `image`, `teamname`) VALUES 
+        ('18', '35', '1987-09-03', '455', '1', 'Batsman', '12', '98', '134', '1/5', 'Dawid Malan', 'malan.png', 'England')
+        ('19', '29', '1991-02-20', '4500', '150', 'All-rounder', '150', '2', '150*', '5-20', 'Ben Stokes', 'stokes.png', 'England'),
+       ('20', 25, '1995-09-15', '5100', '170', 'Batsman', '100', '9', '160*', NULL, 'Joe Root', 'root.png', 'England'),
+       ('21', 24, '1997-12-10', '4300', '150', 'All-rounder', '90', '12', '70*', '4-40', 'Jofra Archer', 'archer.png', 'England'),
+       ('22', 28, '1992-05-15', '6500', '120', 'Batsman', '140', '5', '220*', NULL, 'Rory Burns', 'burns.png', 'England'),
+       ('23', 25, '1995-11-15', '4600', '180', 'Batsman', '100', '11', '140*', NULL, 'Ollie Pope', 'pope.png', 'England'),
+       ('24', 29, '1991-09-20', '5800', '160', 'Batsman', '130', '8', '170*', NULL, 'Jonny Bairstow', 'bairstow.png', 'England'),
+       ('25', 26, '1994-07-01', '4700', '190', 'All-rounder', '120', '7', '100*', '4-50', 'Chris Woakes', 'woakes.png', 'England'),
+       ('26', 27, '1993-02-10', '5300', '150', 'Batsman', '110', '4', '160*', NULL, 'Jos Buttler', 'buttler.png', 'England'),
+       ('27', 30, '1990-06-20', '4800', '170', 'All-rounder', '160', '6', '120*', '5-40', 'Sam Curran', 'curran.png', 'England'),
+       ('28', 29, '1991-01-01', '4900', '180', 'Batsman', '130', '3', '150*', NULL, 'Eoin Morgan', 'morgan.png', 'England'),
+       ('29', 28, '1992-03-15', '5000', '190', 'All-rounder', '140', '2', '110*', '4-30', 'Moeen Ali', 'ali.png', 'England'),
+       ('30', 24, '1997-05-20', '5200', '200', 'All-rounder', '90', '1', '80*', '5-20', 'Tom Curran', 'tcurran.png', 'England');
+
+
+        ('31','26', '1994-03-25', '7000','100', 'Batsman', 120, 3, '250*', NULL, 'Steve Smith', 'smith.png', 'Australia'),
+       ('32', '29', '1991-08-05', '7300', '160', 'Batsman', 130, 8, '190*', NULL, 'David Warner', 'warner.png', 'Australia'),
+       ('33', '25', '1995-09-15', '5100', '170', 'Batsman', 100, 9, '160*', NULL, 'Marnus Labuschagne', 'labuschagne.png', 'Australia'),
+       ('34', '27', '1993-04-30', '5800','180', 'Batsman', 110, 4, '180*', NULL, 'Usman Khawaja', 'khawaja.png', 'Australia'),
+       ('35', '28', '1992-05-15', '6500', '120', 'Batsman', 140, 5, '220*', NULL, 'Cameron Bancroft', 'bancroft.png', 'Australia'),
+       ('36', '30', '1990-06-01', '4800', '200', 'All-rounder', 160, 6, '120*', '6-50', 'Marcus Stoinis', 'stoinis.png', 'Australia'),
+       ('37', '24', '1996-07-10', '4200', '130', 'All-rounder', 90, 7, '80*', '4-40', 'Ashton Agar', 'agar.png', 'Australia'),
+       ('38', '29', '1991-08-05', '7300', '160', 'Batsman', 130, 8, '190*', NULL, 'Travis Head', 'head.png', 'Australia'),
+       ('39', '25', '1995-09-15', '5100', '170', 'Batsman', 100, 9, '160*', NULL, 'Matthew Wade', 'wade.png', 'Australia'),
+       ('40', '27', '1993-10-20', '6700', '180', 'Batsman', 120, 10, '210*', NULL, 'Joe Burns', 'burns.png', 'Australia'),
+       ('41', '28', '1992-11-01', '4500', '190', 'All-rounder', 140, 11, '110*', '5-30', 'Mitchell Starc', 'starc.png', 'Australia'),
+       ('42', '24', '1997-12-10', '4300', '150', 'All-rounder', 90, 12, '70*', '4-40', 'Patrick Cummins', 'cummins.png', 'Australia');
+
+
+
+      (43, 27, '1993-04-30', 5800, 180, 'Batsman', 110, 4, '180*', NULL, 'Quinton de Kock', 'dekock.png', 'South Africa'),
+       (44, 29, '1991-08-05', 7300, 160, 'Batsman', 130, 8, '190*', NULL, 'Faf du Plessis', 'duplessis.png', 'South Africa'),
+       (45, 25, '1995-09-15', 5100, 170, 'Batsman', 100, 9, '160*', NULL, 'Temba Bavuma', 'bavuma.png', 'South Africa'),
+       (46, 26, '1994-03-25', 7000, 100, 'Batsman', 120, 3, '250*', NULL, 'Rassie van der Dussen', 'vanderdussen.png', 'South Africa'),
+       (47, 28, '1992-05-15', 6500, 120, 'Batsman', 140, 5, '220*', NULL, 'Aiden Markram', 'markram.png', 'South Africa'),
+       (48, 30, '1990-06-01', 4800, 200, 'All-rounder', 160, 6, '120*', '6-50', 'Andile Phehlukwayo', 'phehlukwayo.png', 'South Africa'),
+       (49, 24, '1996-07-10', 4200, 130, 'All-rounder', 90, 7, '80*', '4-40', 'Kagiso Rabada', 'rabada.png', 'South Africa'),
+       (50, 29, '1991-08-05', 7300, 160, 'Batsman', 130, 8, '190*', NULL, 'Dwaine Pretorius', 'pretorius.png', 'South Africa'),
+       (51, 25, '1995-09-15', 5100, 170, 'Batsman', 100, 9, '160*', NULL, 'Janneman Malan', 'malan.png', 'South Africa'),
+       (52, 27, '1993-10-20', 6700, 180, 'Batsman', 120, 10, '210*', NULL, 'Heinrich Klaasen', 'klaasen.png', 'South Africa'),
+       (53, 28, '1992-11-01', 4500, 190, 'All-rounder', 140, 11, '110*', '5-30', 'Lungi Ngidi', 'ngidi.png', 'South Africa'),
+       (54, 24, '1997-12-10', 4300, 150, 'All-rounder', 90, 12, '70*', '4-40', 'Anrich Nortje', 'nortje.png', 'South Africa');
